aeed19c04913958a9ffcb0c2ac59e123aaeed785
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-03-05 19:32:49 -0800

    add board methods

--- script.js
+++ script.js
@@ -12,13 +12,42 @@ class Card {
     this.isFaceup = true;
   }
 }
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 class Board {
   constructor(cards) {
     this.grid = this.populate(cards);
   }
 
-  populate(cards) {}
+  populate(cards) {
+    const cardsArray = [...cards, ...cards];
+    shuffle(cardsArray);
+    console.log(cardsArray);
+
+    let grid = [[cardsArray.slice(0, 3)], [cardsArray.slice(cards.length)]];
+
+    console.log(grid);
+
+    return grid;
+  }
+
   render() {}
   isGameWon() {}
   reveal() {}
@@ -36,3 +65,29 @@ You may want a play loop that runs until the game is over. Inside the loop, you 
     */
   }
 }
+
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+const cards = [1, 2, 3];
+const board = new Board(cards);
+board.render();
+console.log(JSON.stringify(board));
